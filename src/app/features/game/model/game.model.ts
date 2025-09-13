fda0032ec1a6570d58c7b6927dc96dbbdc16a510
Jordy-6 <jper220803@gmail.com>
2025-09-13 17:40:32 +0200

    feat: update game model

--- src/app/features/game/model/game.model.ts
+++ src/app/features/game/model/game.model.ts
@@ -2,12 +2,25 @@ export interface Game {
   id: number;
   title: string;
   genre: string;
-  desription: string;
+  description: string;
   platform: string;
   releaseDate: Date;
   rating: number; // Note sur 10
   coverImageUrl: string;
-  status: 'completed' | 'playing' | 'wishlist' | 'abandoned';
+  status: 'completed' | 'playing' | 'wishlist' | 'abandoned' | 'none';
   createdAt: Date;
+  updatedAt?: Date;
+}
+
+export interface UserGameData {
+  userId: number;
+  gameId: number;
+  rating: number;
+  status: 'completed' | 'playing' | 'wishlist' | 'abandoned' | 'none';
   updatedAt: Date;
+}
+
+export interface UpdateUserGameData {
+  rating?: number;
+  status?: 'completed' | 'playing' | 'wishlist' | 'abandoned' | 'none';
 }
