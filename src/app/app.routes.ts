f7112e87ef98d4df5c8ca15f162cb4e0a3ea8ee3
Jordy-6 <jper220803@gmail.com>
2025-09-13 18:40:05 +0200

    feat: add games routes to app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,20 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/games',
+    pathMatch: 'full',
+  },
+  {
     path: 'auth',
     loadChildren: () => import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
   },
   {
     path: 'user',
     loadChildren: () => import('./features/user/user.routes').then((m) => m.USER_ROUTES),
+  },
+  {
+    path: 'games',
+    loadChildren: () => import('./features/game/game.routes').then((m) => m.GAME_ROUTES),
   },
 ];
