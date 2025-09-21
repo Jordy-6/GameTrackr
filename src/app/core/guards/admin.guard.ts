b37af8df4bcb27dd15a8628253df4a87a7b62cc9
Jordy-6 <jper220803@gmail.com>
2025-09-21 22:01:23 +0200

    feat: create auth and admin guards

---
+++ src/app/core/guards/admin.guard.ts
@@ -0,0 +1,23 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../../features/auth/services/auth';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+
+export const AdminGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const currentUser = authService.getCurrentUserSignal();
+
+  return toObservable(currentUser).pipe(
+    map((user) => {
+      if (user && user.role === 'admin') {
+        return true;
+      } else {
+        router.navigate(['/']);
+        return false;
+      }
+    }),
+  );
+};
